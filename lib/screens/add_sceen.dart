5b488abcc98ef6bb76608aabaa2962a3fbede0c3
talhakara <talhakara6734@gmail.com>
2024-06-26 12:12:46 +0300

    add screen push

--- lib/screens/add_sceen.dart
+++ lib/screens/add_sceen.dart
@@ -4,6 +4,7 @@ class AddScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
+
       child: Text(
         'Ekleme Sayfası',
         style: TextStyle(fontSize: 24),
